19e01cb2a52ff5ed986cb85d3929731f52e9f756
Renzo San Juan <rcsanjuan96@gmail.com>
2023-06-10 06:53:33 -0700

    fixed error messages on server console

--- helpers/update_db.js
+++ helpers/update_db.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-module.exports = function updateDatabase(fileStr, noteTitle) {
+module.exports = function updateDatabase(fileStr) {
     fs.writeFile(`./db/db.json`, fileStr, (err) =>
     err
     ? console.error(err)
-    : console.log(`Review for ${noteTitle} has been written to JSON file`)
+    : console.log("Database successfully updated")
     )
 }
